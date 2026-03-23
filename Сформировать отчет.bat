e3def8585d94c65e29f85cdaa6d57f667575f726
Justiks1337 <123255294+Justiks1337@users.noreply.github.com>
2026-03-23 19:18:27 +0400

    f

--- Сформировать отчет.bat
+++ Сформировать отчет.bat
@@ -1,5 +1,5 @@
 @echo off
-chcp 1251 >nul
+chcp 866 >nul
 setlocal enabledelayedexpansion
 
 
@@ -90,8 +90,9 @@ if exist "C:\Program Files\1C\1CE\components" (
 )
 echo    - Список компонентов сохранен в installed_versions.txt
 
-if /i "%COLLECTION_MODE%"=="full" (
 
+:: В FULL режиме выполняем дополнительные шаги
+if /i "%COLLECTION_MODE%"=="full" (
     echo [3/8] Копирование логов 1С...
     :: Формируем путь к папке с логами
     set "USER_PATH=%HOMEDRIVE%%HOMEPATH%"
@@ -286,115 +287,6 @@ if /i "%COLLECTION_MODE%"=="full" (
     tasklist /fo csv 2>nul | find /c /v "" >> "%PROCESSES_FILE%"
 
     echo    - Информация о процессах сохранена в processes.txt (CSV формат)
-
-    echo [6/8] Копирование рабочих пространств (recentworkspace)...
-
-    REM === НАСТРОЙКИ ===
-    set "INPUT_FILE=%USERPROFILE%\1c-enterprise-element\.storage\recentworkspace.json"
-    set "WORKSPACE_DIR=%COMPUTER_PATH%\workspaces"
-    REM =================
-
-    if not exist "%WORKSPACE_DIR%" mkdir "%WORKSPACE_DIR%"
-
-    echo    - Поиск конфига: "%INPUT_FILE%"
-
-    if not exist "%INPUT_FILE%" (
-        echo    - [ОШИБКА] Файл recentworkspace.json не найден!
-        echo Файл recentworkspace.json не найден > "%WORKSPACE_DIR%\not_found.txt"
-        goto :SkipWorkspaces
-    )
-
-    REM Читаем содержимое файла в переменную (JSON в одну строку)
-    for /f "usebackq tokens=*" %%A in ("%INPUT_FILE%") do (
-        set "JSON_CONTENT=%%A"
-    )
-
-    REM --- РАЗБОР JSON ---
-    REM Удаляем шапку {"recentRoots":[ и хвост ]}
-    set "JSON_CONTENT=!JSON_CONTENT:*recentRoots=!"
-    set "JSON_CONTENT=!JSON_CONTENT:*[=!"
-    set "JSON_CONTENT=!JSON_CONTENT:]}=!"
-    set "JSON_CONTENT=!JSON_CONTENT:"=!"
-
-    REM Теперь JSON_CONTENT: file:///c%3A/Path1,file:///c%3A/Path2,...
-
-    :ParseLoopWS
-    for /f "tokens=1* delims=," %%a in ("!JSON_CONTENT!") do (
-        set "RAW_PATH=%%a"
-        set "JSON_CONTENT=%%b"
-
-        REM --- ДЕКОДИРОВАНИЕ URI -> WINDOWS-ПУТЬ ---
-        set "WIN_PATH=!RAW_PATH:file:///=!"
-        set "WIN_PATH=!WIN_PATH:%%3A=:!"
-        set "WIN_PATH=!WIN_PATH:%%20= !"
-        set "WIN_PATH=!WIN_PATH:/=\!"
-
-        echo    - Обработка: !WIN_PATH!
-
-        REM --- Проверяем: папка или файл? ---
-        if exist "!WIN_PATH!\*" (
-            REM ===== ЭТО ПАПКА (проект) =====
-            for %%F in ("!WIN_PATH!") do set "PROJECT_NAME=%%~nxF"
-            set "TARGET_DIR=!WORKSPACE_DIR!\!PROJECT_NAME!"
-            if not exist "!TARGET_DIR!" mkdir "!TARGET_DIR!"
-
-            echo      [ПАПКА] Копирование содержимого в "!TARGET_DIR!"
-            xcopy "!WIN_PATH!" "!TARGET_DIR!\" /e /i /y /q >nul
-            if errorlevel 1 (
-                echo      - ОШИБКА при копировании папки
-            ) else (
-                echo      - Папка скопирована успешно
-            )
-
-        ) else if exist "!WIN_PATH!" (
-            REM ===== ЭТО ФАЙЛ =====
-            for %%F in ("!WIN_PATH!") do set "FILENAME=%%~nxF"
-            set "TARGET_FILE=!WORKSPACE_DIR!\!FILENAME!"
-
-            REM Проверяем, существует ли уже файл с таким именем
-            if exist "!TARGET_FILE!" (
-                REM Если существует, добавляем числовой суффикс
-                set "base=!FILENAME:~0,-4!"
-                set "ext=!FILENAME:~-4!"
-                if "!ext!"==".!ext!" (
-                    rem с расширением
-                    set "counter=1"
-                    :loop_file
-                    if exist "!WORKSPACE_DIR!\!base!_!counter!!ext!" (
-                        set /a counter+=1
-                        goto loop_file
-                    )
-                    set "TARGET_FILE=!WORKSPACE_DIR!\!base!_!counter!!ext!"
-                ) else (
-                    rem без расширения
-                    set "counter=1"
-                    :loop_file_noext
-                    if exist "!WORKSPACE_DIR!\!base!_!counter!" (
-                        set /a counter+=1
-                        goto loop_file_noext
-                    )
-                    set "TARGET_FILE=!WORKSPACE_DIR!\!base!_!counter!"
-                )
-            )
-
-            echo      [ФАЙЛ] Копирование в "!TARGET_FILE!"
-            copy "!WIN_PATH!" "!TARGET_FILE!" /y >nul
-            if errorlevel 1 (
-                echo      - ОШИБКА при копировании файла
-            ) else (
-                echo      - Файл скопирован успешно
-            )
-
-        ) else (
-            echo      [НЕ НАЙДЕНО] !WIN_PATH!
-        )
-
-        if defined JSON_CONTENT goto ParseLoopWS
-    )
-
-    echo    - Копирование рабочих пространств завершено
-
-    :SkipWorkspaces
 )
 
 echo [7/8] Сбор информации о Java...
